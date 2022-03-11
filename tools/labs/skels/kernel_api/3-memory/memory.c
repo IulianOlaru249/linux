483b0e4fc4814ec0f25f5daf05d04025d13839b7
Iulian Olaru <iulianolaru249@yahoo.com>
2022-03-11 09:21:46 +0200

    Solve 3rd exercise
    root@qemux86:~/skels/kernel_api# rmmod 3-memory/memory.ko
    pid: 228, timestamp: -55002
    pid: 0, timestamp: -55002
    pid: 1, timestamp: -55002
    pid: 2, timestamp: -55002
    
    Signed-off-by: Iulian Olaru <iulianolaru249@yahoo.com>

--- tools/labs/skels/kernel_api/3-memory/memory.c
+++ tools/labs/skels/kernel_api/3-memory/memory.c
@@ -25,29 +25,52 @@ static struct task_info *task_info_alloc(int pid)
 	struct task_info *ti;
 
 	/* TODO 1: allocated and initialize a task_info struct */
+	ti = kmalloc(sizeof(*ti), GFP_KERNEL);
+	if (ti == NULL)
+		return NULL;
+
+	ti->pid = pid;
+	ti->timestamp = jiffies;
 
 	return ti;
 }
 
 static int memory_init(void)
 {
+	struct task_struct *p;
+	p = get_current();
+
 	/* TODO 2: call task_info_alloc for current pid */
+	ti1 = task_info_alloc(p->pid);
 
 	/* TODO 2: call task_info_alloc for parent PID */
+	p = next_task(p);
+	ti2 = task_info_alloc(p->pid);
 
 	/* TODO 2: call task_info alloc for next process PID */
+	p = next_task(p);
+	ti3 = task_info_alloc(p->pid);
 
 	/* TODO 2: call task_info_alloc for next process of the next process */
+	p = next_task(p);
+	ti4 = task_info_alloc(p->pid);
 
 	return 0;
 }
 
 static void memory_exit(void)
 {
-
 	/* TODO 3: print ti* field values */
+	printk("pid: %d, timestamp: %ld\n", ti1->pid, ti1->timestamp);
+	printk("pid: %d, timestamp: %ld\n", ti2->pid, ti2->timestamp);
+	printk("pid: %d, timestamp: %ld\n", ti3->pid, ti3->timestamp);
+	printk("pid: %d, timestamp: %ld\n", ti4->pid, ti4->timestamp);
 
 	/* TODO 4: free ti* structures */
+	kfree(ti1);
+	kfree(ti2);
+	kfree(ti3);
+	kfree(ti4);
 }
 
 module_init(memory_init);
