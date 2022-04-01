7dd6f1e55bbda87d1bf4d92cd96058c49948e8a6
Iulian Olaru <iulianolaru249@yahoo.com>
2022-04-01 09:08:55 +0300

    Solve 1st exercice
    [timer_init] Init module
    Got Here!
    Got Here!
    root@qemux86:~# insmod skels/deferred_work/1-2-timer/timer.ko Got Here!
    root@qemux86:~# rmmod skels/deferred_work/1-2-timer/timer.ko
    [timer_exit] Exit module
    
    Signed-off-by: Iulian Olaru <iulianolaru249@yahoo.com>

--- tools/labs/skels/deferred_work/1-2-timer/timer.c
+++ tools/labs/skels/deferred_work/1-2-timer/timer.c
@@ -8,6 +8,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/jiffies.h>
 
 MODULE_DESCRIPTION("Simple kernel timer");
 MODULE_AUTHOR("SO2");
@@ -17,10 +18,12 @@ MODULE_LICENSE("GPL");
 
 static struct timer_list timer;
 
+unsigned long current_jiffies, next_jiffies;
+
 static void timer_handler(struct timer_list *tl)
 {
 	/* TODO 1: print a message */
-	pr_info("Got Here!");
+	pr_info("Got Here!\n");
 
 	/* TODO 2: rechedule timer */
 }
@@ -30,8 +33,10 @@ static int __init timer_init(void)
 	pr_info("[timer_init] Init module\n");
 
 	/* TODO 1: initialize timer */
+	timer_setup(&timer, timer_handler, 0);
 
 	/* TODO 1: schedule timer for the first time */
+	mod_timer(&timer, jiffies + TIMER_TIMEOUT * HZ);
 
 	return 0;
 }
@@ -41,6 +46,7 @@ static void __exit timer_exit(void)
 	pr_info("[timer_exit] Exit module\n");
 
 	/* TODO 1: cleanup; make sure the timer is not running after we exit */
+	del_timer_sync(&timer);
 }
 
 module_init(timer_init);
